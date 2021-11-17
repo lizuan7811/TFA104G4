aa4a26ecf156605aba9c068b394367f4c0d809ea
ChenHan <linhanhna89072@gmail.com>
2021-11-17 17:28:31 +0800

    test

---
+++ src/main/java/hantest/Test.java
@@ -0,0 +1,12 @@
+package hantest;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
