08e984f402d3691596d77f1340bb209991984562
lizuan7811 <lizuan7811@users.noreply.github.com>
2021-12-09 09:28:03 +0800

    build photo update

--- src/main/webapp/js/dividePage.js
+++ src/main/webapp/js/dividePage.js
@@ -5,7 +5,15 @@ $(function(){
 		type:"POST",
 		success:function(data)
 		{
-			console.log(data);	
+			console.log(data);
+			var daObj=JSON.parse(data);
+			for(var key in daObj)
+			{
+				// console.log("data[key]\t"+daObj[key])
+				var picStr="data:image/jpg;base64,"+daObj[key];
+				var divblock="<img class='inWinPic'src=\""+picStr+"\"></div>";
+				$(".winCenter").after(divblock);
+			}
 		},
 		error:function(){
 			
